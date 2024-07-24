403effa66629eb6016160b2384c44728ef3d17fb
ReginaZaets <regina.alibaeva21@gmail.com>
2024-07-24 20:09:55 +0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+    'react',
+    'react-hooks',
+  ],
+  rules: {
+    // ваши правила
+  },
+};
---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,11 @@
+import Header from "../components/common/Header"
+
+const MainPage = () => {
+  return (
+    <div>
+      <Header/>
+    </div>
+  )
+}
+
+export default MainPage
