477a44e32da1aaf4648e556245ba5195612c70b0
Karpova Julia <am_julia@mail.ru>
2024-07-25 12:31:23 +0600

    +

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -4,7 +4,9 @@ const Header = () => {
       <div className="flex justify-between py-12">
         <div>
           <img src="/images/logo.svg" alt="imageLogo" width={220} height={35} />
-          <p className="py-4 text-lg text-black font-normal">Онлайн-тренировки для занятий дома</p>
+          <p className="py-4 text-lg text-black font-normal">
+            Онлайн-тренировки для занятий дома
+          </p>
         </div>
         <button className="bg-btnColor rounded-small w-btnWidth h-btnHeight text-black text-lg">
           Войти
