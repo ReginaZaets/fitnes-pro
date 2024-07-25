a898a7deaa36d94493d1b77427193a7587955c11
rambak02 <rambak02@mail.ru>
2024-07-26 02:23:52 +0700

    визуальное добавление кнопки "Добавить курс"

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -10,9 +10,9 @@ const MainPage = () => {
         <div className="text-6xl w-5/6 h-32 text-black font-medium leading-[60px]">
           Начните заниматься спортом и улучшите качество жизни
         </div>
-        <div className="relative max-w-xs p-4 bg-[#BCEC30] text-black rounded-lg w-[288px] flex items-center ">
-          <div className="absolute top-[94%] transform -translate-y-1/1 w-4 h-4 bg-[#BCEC30] rotate-45 "></div>{" "}
-          <p>Измени своё тело за пол года</p>
+        <div className="relative max-w-xs p-3 bg-[#BCEC30] text-black rounded-lg w-[288px] flex items-center ">
+          <div className="absolute top-[92%] transform -translate-y-1/1 w-4 h-4 bg-[#BCEC30] rotate-45 "></div>{" "}
+          <p>Измени своё тело за полгода!</p>
         </div>
       </div>
       <div className="flex flex-row flex-wrap items-center gap-[30px] pl-[6%] ml-[5%] ">
