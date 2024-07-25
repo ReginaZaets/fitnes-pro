0d1f01124663b786b2afad8ff1f21234a6de7969
ReginaZaets <regina.alibaeva21@gmail.com>
2024-07-25 08:30:07 +0300

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 export default {
   content: ["./src/**/*.{html,js,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: "480px",
+        md: "768px",
+        lg: "976px",
+        xl: "1440px",
+      },
+      colors: {
+        mainColor: "#FAFAFA",
+        btnColor: "#BCEC30",
+      },
+      borderRadius: {
+        small: "46px",
+      },
+      width: {
+        btnWidth: "103px",
+      },
+      height: {
+        btnHeight: "52px",
+      },
+    },
   },
   plugins: [],
 };
