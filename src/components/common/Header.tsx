ec15d817e6aeacab9e9124494d9aec8eeb952bee
Karpova Julia <am_julia@mail.ru>
2024-07-25 16:10:02 +0600

    добавила прозрачность в тексте под логотипом

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -5,8 +5,8 @@ const Header = () => {
     <div className="w-contentWidth my-0 mx-auto ">
       <div className="flex justify-between py-12">
         <div>
-          <img src="/images/logo.svg" alt="imageLogo" width={220} height={35} />
-          <p className="py-4 text-lg text-black font-normal">
+          <img src="/images/logo.svg" alt="imageLogo" />
+          <p className="py-4 text-lg text-black font-normal opacity-50">
             Онлайн-тренировки для занятий дома
           </p>
         </div>
--- src/components/common/User.tsx
+++ src/components/common/User.tsx
@@ -1,28 +1,28 @@
 const User = () => {
   return (
     <div className="flex gap-x-3 items-center relative">
-
       <button className="bg-btnColor rounded-small w-btnWidth h-btnHeight text-black text-lg">
         Войти
       </button>
-
       <div className="hidden">
-      <img src="/images/Profile.svg" alt="profile" width={50} height={50} />
-      <p className="py-4 text-2xl text-black font-normal">Сергей</p>
-      <svg
-        className="mx-2"
-        width="14"
-        height="9"
-        viewBox="0 0 14 9"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M12.3552 1.03308L6.67761 6.7107L0.999999 1.03308"
-          stroke="black"
-          stroke-width="2"
-        />
-      </svg>
+        <div className="flex gap-x-3 items-center relative">
+          <img src="/images/Profile.svg" alt="profile" />
+          <p className="py-4 text-2xl text-black font-normal">Сергей</p>
+          <svg
+            className="mx-2"
+            width="14"
+            height="9"
+            viewBox="0 0 14 9"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M12.3552 1.03308L6.67761 6.7107L0.999999 1.03308"
+              stroke="black"
+              stroke-width="2"
+            />
+          </svg>
+        </div>
       </div>
 
       <div className="hidden absolute top-24 right-0 rounded-3xl bg-white w-userPopWidth h-userPopHeight">
