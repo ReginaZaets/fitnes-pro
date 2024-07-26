2c5831818c7d95c99587cf4938814ef421c2cad0
Karpova Julia <am_julia@mail.ru>
2024-07-26 10:43:51 +0600

    страница userProfile

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,9 +1,11 @@
 import Header from "../components/common/Header"
+import UserProfile from "./UserProfile"
 
 const MainPage = () => {
   return (
     <div className="bg-mainColor ">
       <Header/>
+      < UserProfile/>
     </div>
   )
 }
