189787d79e883a5ef057fd538e8447ba3a705d43
Karpova Julia <am_julia@mail.ru>
2024-07-26 10:25:30 +0600

    +

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,9 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
+   
    ],
-  parser: "@typescript-eslint/parser",
+  
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -16,8 +14,5 @@ module.exports = {
     ecmaVersion: 2021,
     sourceType: "module",
   },
-  plugins: ["@typescript-eslint", "react", "react-hooks", "prettier"],
-  rules: {
-   
-  },
+  
 };
