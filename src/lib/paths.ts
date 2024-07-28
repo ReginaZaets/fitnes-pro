194c8806047d02299bdad9c7c099ab37f15f84f4
fedotovps <fedotovps@bk.ru>
2024-07-28 16:53:43 +0300

    Окончательная структура роутов, изменнеие маршрутов

--- src/lib/paths.ts
+++ src/lib/paths.ts
@@ -1,11 +1,13 @@
 export const paths = {
   MAIN: "/",
-  SIGN_IN: "signin",
-  SIGN_UP: "signup",
-  RESET_PASSWORD: "/reset-password",
-  NEW_PASSWORD: "/new-password",
+  SIGN_IN_MODAL: "signin",
+  SIGN_UP_MODAL: "signup",
+  RESET_PASSWORD_MODAL: "/reset-password",
+  NEW_PASSWORD_MODAL: "/new-password",
   COURSE: "/course/:id",
   WORKOUT: "/workout/:id",
+  WORKOUT_PROGRESS_MODAL: "/progress",
   PROFILE: "/profile",
+  WORKOUT_MODAL: "/choose-workout",
   NOT_FOUND: "*",
 };
