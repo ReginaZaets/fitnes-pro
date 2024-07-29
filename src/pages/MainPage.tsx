1aefb18cfe4d2f3f8af3eead7a4cab9437fcecd5
Karpova Julia <am_julia@mail.ru>
2024-07-29 19:58:01 +0600

    поправила хедер

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,10 +1,8 @@
 import Header from "../components/common/Header";
-import UserProfile from "./UserProfile";
 const MainPage = () => {
   return (
     <div className="bg-mainColor ">
       <Header />
-      <UserProfile/>
     </div>
   );
 };
