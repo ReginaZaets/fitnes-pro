cc78dc6a1b866656f4812ecfecfad2cc856e2f6a
rambak02 <rambak02@mail.ru>
2024-07-29 16:22:27 +0700

    подправил стили

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -6,20 +6,22 @@ const MainPage = () => {
   return (
     <div className="bg-mainColor flex flex-col justify-center items-center">
       <Header />
-      <div className="flex gap-[5px] font-normal  text-2xl w-[79%] items-center my-4 ">
-        <div className="text-6xl w-5/6 h-32 text-black font-medium leading-[60px]">
-          Начните заниматься спортом и улучшите качество жизни
+      <main>
+        <div className="flex gap-[5px] font-normal  text-2xl items-center my-4 w-full">
+          <div className=" w-[947px] h-[120px] text-black text-[60px] font-medium leading-[60px] text-left">
+            Начните заниматься спортом и улучшите качество жизни
+          </div>
+          <div className="relative max-w-xs p-3 bg-[#BCEC30] text-black rounded-lg w-[288px] flex items-center ">
+            <div className="absolute top-[88%]  rotate-45 w-0 h-0 border-l-[10px] border-r-[10px] border-t-[30px] border-l-transparent border-r-transparent border-t-[#BCEC30] "></div>{" "}
+            <p>Измени своё тело за полгода!</p>
+          </div>
         </div>
-        <div className="relative max-w-xs p-3 bg-[#BCEC30] text-black rounded-lg w-[288px] flex items-center ">
-          <div className="absolute top-[88%]  rotate-45 w-0 h-0 border-l-[10px] border-r-[10px] border-t-[30px] border-l-transparent border-r-transparent border-t-[#BCEC30] "></div>{" "}
-          <p>Измени своё тело за полгода!</p>
+        <div className="flex flex-row flex-wrap items-center gap-[30px]">
+          {courses.map((course) => (
+            <CourseCard key={course.name} course={course} />
+          ))}
         </div>
-      </div>
-      <div className="flex flex-row flex-wrap items-center gap-[30px] pl-[6%] ml-[5%] ">
-        {courses.map((course) => (
-          <CourseCard key={course.name} course={course} />
-        ))}
-      </div>
+      </main>
     </div>
   );
 };
