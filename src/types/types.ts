8abc746525109115b5d53415c201d1a3ea00e774
fedotovps <fedotovps@bk.ru>
2024-07-29 15:09:22 +0300

    Типизация для объекта тренировки и упражнения

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,3 +1,4 @@
+// Типизация для объекта курса
 export type Course = {
   _id: string;
   nameRU: string;
@@ -8,3 +9,17 @@ export type Course = {
   order: number;
   workouts: [];
 };
+
+// Типизация для объекта тренировки
+export type Workout = {
+  _id: string;
+  exercises: Exercise[];
+  name: string;
+  video: string;
+}
+
+// Типизация для объекта упражнения
+export type Exercise = {
+  name: string;
+  quantity: number;
+}
