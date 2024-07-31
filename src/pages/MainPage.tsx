a2daa32d7d0989fc663c2ffbaf67c857927b5ab4
Andrey-Bakin <andybulkin@yandex.ru>
2024-07-31 14:48:51 +0300

    +

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,10 +1,8 @@
 import Header from "../components/common/Header";
-import Workout from "../components/common/Workout";
 const MainPage = () => {
   return (
     <div className="bg-mainColor ">
       <Header />
-      <Workout />
     </div>
   );
 };
