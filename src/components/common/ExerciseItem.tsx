f92c498195c908e7ca54cb3ca6e7d2e2a79f62cf
Andrey-Bakin <andybulkin@yandex.ru>
2024-07-31 16:52:23 +0300

    добавлен динамический progress

--- src/components/common/ExerciseItem.tsx
+++ src/components/common/ExerciseItem.tsx
@@ -6,12 +6,12 @@ type PropsExerciseItem = {
 const ExerciseItem = ({ title, progress }: PropsExerciseItem) => {
     return (
         <div className="w-80 h-9 text-left">
-        <h3 className="font-[Roboto] text-[18px] leading-[110%] font-normal">{title}0%</h3>
+        <h3 className="font-[Roboto] text-[18px] leading-[110%] font-normal">{title} {progress}0%</h3>
         <input
-          value=""
+          value={progress}
           max="100"
           className="w-full h-[6px] mt-[10px] block"
-          {progress}
+          
         />
       </div>
     );
