6c74202842819c1a9047372d7a1f49e4048d88a7
Andrey-Bakin <andybulkin@yandex.ru>
2024-08-05 14:54:23 +0300

    добавлении функции получения тренировок

--- src/components/common/WorkoutComponent.tsx
+++ src/components/common/WorkoutComponent.tsx
@@ -1,52 +1,70 @@
+import { Link, useParams } from "react-router-dom";
 import { Exercise } from "../../lib/exercises";
 // import { Workout } from "../../lib/workout";
 // import WorkoutItem from "../popups/workoutPopups/WorkoutItem";
 import ExerciseItem from "./ExerciseItem";
-
-
-
+import { paths } from "../../lib/paths";
+import { useEffect, useState } from "react";
+import { fetchGetWorkouts } from "../../api/coursesApi";
+import { Workout } from "../../types/types";
 
 const WorkoutComponent = () => {
-
   const NameSelectedCourse = "Йога";
   const workoutNumber = "2";
   const topicWorkout = "Красота и здоровье";
   const titleWorkout = "Йога на каждый день / 2 день";
-  const video = "https://www.youtube.com/embed/v-xTLFDhoD0"
+  const video = "https://www.youtube.com/embed/v-xTLFDhoD0";
 
+  const [allWorkouts, setAllWorkouts] = useState<Workout[]>([]);
+
+  const workoutID = "hfgxlo";
+
+  useEffect(() => {
+    fetchGetWorkouts().then((data) => {
+      setAllWorkouts(data);
+    });
+  }, []);
 
   return (
-    <main className="flex flex-col justify-start gap-10">
-      <div className="w-[810px] h-[119px] flex flex-col justify-start">
+    <main className="flex flex-col justify-start gap-10 ">
+      <div className="w-[810px] h-[119px] flex flex-col justify-start ">
         <h1 className="font-[Roboto] text-[60px] font-medium text-black ">
-        {NameSelectedCourse}
+          {NameSelectedCourse}
         </h1>
-        <h3 className="font-[Roboto] text-[32px] font-normal">
+        <h3 className="font-[Roboto] text-[32px] font-normal ">
           {topicWorkout} / {titleWorkout}
         </h3>
       </div>
-      <div className="aspect-w-16 aspect-h-9 relative max-h-[639px] w-full overflow-hidden rounded-[30px] shadow-[customShadow]">
-      <iframe
+      <div className="max-h-[639px] max-w-[1160px] w-full overflow-hidden rounded-[30px] shadow-[customShadow] ">
+        <iframe
           width="1160"
           height="639"
           src={video}
           title="YouTube video player"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           allowFullScreen
-      ></iframe>
+        ></iframe>
       </div>
-      <div className="rounded-[30px] bg-white shadow-[customShadow] w-full h-[395px] p-10 flex flex-col justify-start items-start">
+      <div className="rounded-[30px] bg-white shadow-[customShadow] w-full h-[395px] p-10 flex flex-col justify-start items-start ">
         <h2 className="font-[StratosSkyeng, sans-serif] text-[32px] font-normal mb-5">
           Упражнения тренировки {workoutNumber}
         </h2>
         <div className="mb-[40px] h-[168px] grid grid-cols-1 gap-x-[60px] gap-y-[24px] md:grid-cols-3 md:gap-y-[20px]">
-            {Exercise.map((item) => {
-              return(<ExerciseItem title={item.title} progress={item.progress} />)
-            })}     
+          {Exercise.map((item) => {
+            return (
+              <ExerciseItem
+                key={item.id}
+                name={item.name}
+                quantity={item.quantity}
+              />
+            );
+          })}
         </div>
-        <button className="w-[320px] h-[52px] rounded-[30px] bg-[#BCEC30] font-[Roboto san-serif] text-[18px] font-medium leading-[110%]">
-          <p className="mx-[37px] my-[16px]">Заполнить свой прогресс</p>
-        </button>
+        <Link to={paths.WORKOUT_PROGRESS_MODAL}>
+          <button className="w-[320px] h-[52px] rounded-[30px] bg-[#BCEC30] font-[Roboto san-serif] text-[18px] font-medium leading-[110%]">
+            <p className="mx-[37px] my-[16px]">Заполнить свой прогресс</p>
+          </button>
+        </Link>
       </div>
     </main>
   );
