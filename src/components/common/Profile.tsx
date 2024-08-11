73cc0eaf501be6fbc1ba99635e83b514011d489e
Karpova Julia <am_julia@mail.ru>
2024-08-11 17:33:24 +0600

    устранила ошибки типизации в Профиле и контексте Курсов

--- src/components/common/Profile.tsx
+++ src/components/common/Profile.tsx
@@ -1,16 +1,17 @@
 import { CourseCard } from "./CourseCard";
 import { Link } from "react-router-dom";
 import { paths } from "../../lib/paths";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useUserContext } from "../../context/hooks/useUser";
 import { logout } from "../../api/authUsersApi";
 import { fetchGetCoursesUser } from "../../api/coursesApi";
 import { Course } from "../../types/types";
+import { useUserCoursesContext } from "../../context/hooks/useUserCourses";
 
 const Profile = () => {
   const user = useUserContext();
-  //const {coursesUser, setCoursesUser} = useUserCoursesContext();
-    const [coursesUser, setCoursesUser] = useState<Course[]>([]);
+  const {coursesUser, setCoursesUser} = useUserCoursesContext();
+    //  const [coursesUser, setCoursesUser] = useState<Course[]>([]);
 
   useEffect(() => {
     if (user) {
@@ -65,7 +66,7 @@ const Profile = () => {
       </h2>
 
       <div className="flex flex-row flex-wrap items-center gap-[40px]">
-        {coursesUser.map((course:Course) => (
+        {coursesUser?.map((course:Course) => (
           <CourseCard key={course._id} course={course} />
         ))}
       </div>
