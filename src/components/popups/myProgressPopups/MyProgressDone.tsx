60602db68f08d97d79b8eada22f973d8d36c64fa
Andrey-Bakin <andybulkin@yandex.ru>
2024-08-13 13:09:52 +0300

    отображение модалки засчитанного прогреса, корректировка стилей

--- src/components/popups/myProgressPopups/MyProgressDone.tsx
+++ src/components/popups/myProgressPopups/MyProgressDone.tsx
@@ -1,26 +1,33 @@
-import { Link } from "react-router-dom";
-import { paths } from "../../../lib/paths";
+import { useState } from "react";
 
 const MyProgressDone = () => {
+  const [isOpenedMyProgressDone, setIsOpenedMyProgressDone] =
+    useState<boolean>(true);
+
+  const toggleModal = () => {
+    setIsOpenedMyProgressDone(false);
+  };
+
   return (
     <>
-      <Link to={paths.WORKOUT}>
-      <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-20 z-10">
-      <div className="absolute bg-white border w-[343px] h-[252px] md:w-[426px] md:h-[278px] shadow-customShadow rounded-radiusModal p-10">
-        <p className="text-[32px] md:text-[40px] leading-[48px] text-center">
-          Ваш прогресс засчитан!
-        </p>
-        <img
-          src="/images/CheckInCircle.png"
-          alt="check"
-          className="w-[68px] h-[68px] mt-[34px] mx-[139px]"
-        />
-      </div>
-    </div>
-      </Link>
-      
+      {isOpenedMyProgressDone && (
+        <div
+          onClick={toggleModal}
+          className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-20 z-10"
+        >
+          <div className="absolute bg-white border w-[343px] h-[252px] md:w-[426px] md:h-[278px] shadow-customShadow rounded-radiusModal p-10">
+            <p className="text-[32px] md:text-[40px] leading-[110%] text-center">
+              Ваш прогресс засчитан!
+            </p>
+            <img
+              src="/images/CheckInCircle.png"
+              alt="check"
+              className="w-[68px] h-[68px] mt-[34px] mx-[97px] md:mx-[139px]"
+            />
+          </div>
+        </div>
+      )}
     </>
-    
   );
 };
 
