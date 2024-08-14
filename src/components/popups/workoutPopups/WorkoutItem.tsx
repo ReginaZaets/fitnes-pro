70b186353a32b3796b2f44591e128c0490a09588
ReginaZaets <regina.alibaeva21@gmail.com>
2024-08-14 16:51:22 +0300

    изменила navigate на link

--- src/components/popups/workoutPopups/WorkoutItem.tsx
+++ src/components/popups/workoutPopups/WorkoutItem.tsx
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 type PropsWorkoutItem = {
   title: string;
@@ -13,16 +13,10 @@ const WorkoutItem = ({
   workoutID,
 }: PropsWorkoutItem) => {
 
-  const navigate = useNavigate();
-
   const parts = title.split("/");
 
   const firstParts = parts[0] || "";
   const secondParts = parts.slice(1).join("/");
-
-  const handleClickPageWorkout = () => {
-    navigate(`/profile/${courseID}/workout/${workoutID}`);
-  };
 
   return (
     <label className="flex items-center border-b-2 border-[#C4C4C4]  w-[354px] py-[10px]">
@@ -30,14 +24,15 @@ const WorkoutItem = ({
         src={done ? "/icones/check.svg" : "/icones/ellipse.svg"}
         alt={done ? "check" : "ellipse"}
       />
-      <div
-        onClick={handleClickPageWorkout}
-        className="flex flex-col justify-center ml-[10px] w-[354px] h-[74px]  cursor-pointer"
-      >
-        <h1 className=" text-2xl leading-[26.4px]">{firstParts}</h1>
-        {secondParts && (
-          <p className="text-base leading-[17.6px] mt-[10px]">{secondParts}</p>
-        )}
+      <div className="flex flex-col justify-center ml-[10px] w-[354px] h-[74px]  cursor-pointer">
+        <Link to={`/profile/${courseID}/workout/${workoutID}`}>
+          <h1 className=" text-2xl leading-[26.4px]">{firstParts}</h1>
+          {secondParts && (
+            <p className="text-base leading-[17.6px] mt-[10px]">
+              {secondParts}
+            </p>
+          )}
+        </Link>
       </div>
     </label>
   );
