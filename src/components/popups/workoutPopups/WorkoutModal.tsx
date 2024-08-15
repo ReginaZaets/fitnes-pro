29069c21e141206edc9d8aa05f3432e76cd9e40b
ReginaZaets <regina.alibaeva21@gmail.com>
2024-08-15 17:56:16 +0300

    добавила стили для адаптива моб версии в WorkoutModal

--- src/components/popups/workoutPopups/WorkoutModal.tsx
+++ src/components/popups/workoutPopups/WorkoutModal.tsx
@@ -42,12 +42,12 @@ const WorkoutModal = ({
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-20 z-10 transition-opacity duration-300">
       <div
         ref={modalRef}
-        className="absolute bg-white border xl:p-10 p-8  w-[460px] h-[610px] shadow-customShadow rounded-radiusModal"
+        className="absolute bg-white border xl:p-10 p-[30px] w-auto h-auto xl:w-[460px] xl:h-[590x] shadow-customShadow rounded-radiusModal"
       >
-        <h1 className="text-[32px] leading-[35.2px] ml-[17px]">
+        <h1 className="text-[32px] leading-[35.2px] xl:ml-[17px] xl:w-[346px] w-[283px]">
           Выберите тренировку
         </h1>
-        <div className=" xl:mt-[48px] mt-[34px] w-[380px] h-[435px] overflow-y-auto">
+        <div className=" xl:mt-[48px] mt-[34px] w-auto h-auto  xl:w-[380px] xl:h-[400px] overflow-y-auto">
           {workoutUsers &&
             workoutUsers.map((item, index) => {
               return (
