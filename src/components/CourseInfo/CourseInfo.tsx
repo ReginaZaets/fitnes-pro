788af29c91d2a4624d5ac8df808a3371f18364c5
fedotovps <fedotovps@bk.ru>
2024-08-16 18:49:54 +0300

    Правка стилей

--- src/components/CourseInfo/CourseInfo.tsx
+++ src/components/CourseInfo/CourseInfo.tsx
@@ -46,7 +46,7 @@ const CourseInfo = () => {
         return "bg-yoga";
       case "Стретчинг":
         return "bg-sterch";
-      case "Зумба":
+      case "Танцевальный фитнес":
         return "bg-zumba";
       case "Степ-аэробика":
         return "bg-step";
@@ -161,7 +161,7 @@ const CourseInfo = () => {
   return (
     <>
       <div
-        className={`mt-3 rounded-[28px] md:w-full ${
+        className={`mt-3 rounded-[28px] md:w-full overflow-hidden ${
           data && getBackgroundColor(data?.nameRU)
         } md: mt-0`}
       >
@@ -172,116 +172,113 @@ const CourseInfo = () => {
           <img src={url} alt="courseColor" className="block" />
         </div>
       </div>
-      <main>
-        <section className="my-[20px] pb-[40px] flex flex-col">
-          <p className="text-[24px] md:text-[40px] text-black font-semibold my-10">
-            Подойдет для вас, если:
-          </p>
-          <section className="flex gap-[17px] flex-col md:flex-row md:flex-wrap md:justify-center md:items-stretch">
-            {data?.fitting.map((item, index) => (
-              <div key={index} className=" card">
-                <div className="flex gap-6 ">
-                  <span className="text-btnColor number-reasons">
-                    {index + 1}
-                  </span>
-                  <p className="text-reasons" key={item}>
-                    {item}
-                  </p>
-                </div>
-              </div>
-            ))}
-          </section>
-        </section>
-        <section className="flex flex-col">
-          <p className="text-[24px] md:text-[40px] text-black font-semibold">
-            Направления
-          </p>
-          <div className="bg-btnColor mt-6 md:mt-[40px] h-full w-full rounded-[28px] p-[30px] flex wd:items-stretch wd:flex-wrap wd: justify-center">
-            <div className="direction">
-              {data?.directions.map((item) => (
-                <span className="directions-name " key={item}>
-                  {item}
+      <section className="my-[20px] pb-[40px] flex flex-col">
+        <p className="text-[24px] md:text-[40px] text-black font-semibold my-10">
+          Подойдет для вас, если:
+        </p>
+        <section className="flex gap-[17px] flex-col md:flex-row md:flex-wrap md:justify-center md:items-stretch">
+          {data?.fitting.map((item, index) => (
+            <div key={index} className=" card">
+              <div className="flex gap-6 ">
+                <span className="text-btnColor number-reasons">
+                  {index + 1}
                 </span>
-              ))}
-            </div>
-          </div>
-        </section>
-        <section className="flex absolute my-[142px]">
-          <div className="flex flex-col z-20 bg-white md:z-0">
-            <p className=" align-center z-40 leading-none text-[32px] md:text-[60px] text-black font-bold ">
-              Начни путь <br />к новому телу
-            </p>
-            <ul className=" p-4 md:w-[437px] md:my-7 flex md:items-start flex-col">
-              <li className="items">проработка всех групп мышц</li>
-              <li className="items">тренировка суставов</li>
-              <li className="items">улучшение циркуляции крови</li>
-              <li className="items">упражнения заряжают бодростью</li>
-              <li className="items">помогают противостоять стрессам</li>
-            </ul>
-            <button
-              disabled={isButtonDisabled}
-              className={`bg-btnColor hover:bg-btnHoverGreen ${
-                isButtonDisabled ? "opacity-70 cursor-not-allowed" : ""
-              } pointer rounded-small w-[283px] md:w-[437px] h-btnHeight text-black text-lg my-[28px]`}
-            >
-              {user ? (
-                isUserCourse ? (
-                  <p onClick={deleteCourse} className="text-[18px]">
-                    Удалить курс
-                  </p>
-                ) : (
-                  <p onClick={addCourse} className="text-[18px]">
-                    Добавить курс
-                  </p>
-                )
-              ) : (
-                <p onClick={openSigninModal} className="text-[18px]">
-                  Войдите, чтобы добавить курс
+                <p className="text-reasons" key={item}>
+                  {item}
                 </p>
-              )}
-            </button>
-            {message && (
-              <p className="text-[18px] text-center text-gray-700">
-                Курс удален
+              </div>
+            </div>
+          ))}
+        </section>
+      </section>
+      <section className="flex flex-col">
+        <p className="text-[24px] md:text-[40px] text-black font-semibold">
+          Направления
+        </p>
+        <div className="bg-btnColor mt-6 md:mt-[40px] h-full w-full rounded-[28px] p-[30px] flex wd:items-stretch wd:flex-wrap wd: justify-center">
+          <div className="direction">
+            {data?.directions.map((item) => (
+              <span className="directions-name " key={item}>
+                {item}
+              </span>
+            ))}
+          </div>
+        </div>
+      </section>
+      <section className="relative flex my-[142px] ">
+        <div className="shadow-lg w-full p-[40px] rounded-[30px] z-50  bg-white flex flex-col items-center z-40 st:z-0 md:justify-start md:items-start">
+          <p className=" align-center z-50 leading-none text-[32px] md:text-[60px] text-black font-bold ">
+            Начни путь <br />к новому телу
+          </p>
+          <ul className=" p-4 md:w-[437px] md:my-7 flex md:items-start flex-col">
+            <li className="items">проработка всех групп мышц</li>
+            <li className="items">тренировка суставов</li>
+            <li className="items">улучшение циркуляции крови</li>
+            <li className="items">упражнения заряжают бодростью</li>
+            <li className="items">помогают противостоять стрессам</li>
+          </ul>
+          <button
+            disabled={isButtonDisabled}
+            className={`bg-btnColor hover:bg-btnHoverGreen ${isButtonDisabled ? "opacity-70 cursor-not-allowed" : ""} pointer rounded-small w-[283px] md:w-[437px] h-btnHeight text-black text-lg my-[28px]`}
+          >
+            {user ? (
+              isUserCourse ? (
+                <p onClick={deleteCourse} className="text-[18px]">
+                  Удалить курс
+                </p>
+              ) : (
+                <p onClick={addCourse} className="text-[18px]">
+                  Добавить курс
+                </p>
+              )
+            ) : (
+              <p onClick={openSigninModal} className="text-[18px]">
+                Войдите, чтобы добавить курс
               </p>
             )}
-          </div>
+          </button>
+          {message && (
+            <p className="text-[18px] text-center text-gray-700">Курс удален</p>
+          )}
+        </div>
+        <div>
+          {" "}
           <img
             src="/images/infoCourse.svg"
             alt=""
-            className="relative z-10 right-[175px] bottom-[270px] md:left-[130px] md:z-10 md:right-[0px] md:bottom-[100px]"
+            className="absolute z-10 bottom-[240px] left-[85px] st:left-[600px] st:z-10 st:right-[0px] st:bottom-[100px] w-[80%] st:w-auto "
           />
           <img
             src="/images/vector1.svg"
             alt=""
-            className="relative bottom-[357px] right-[470px] md:bottom-[250px] md:right-[300px]"
+            className="absolute bottom-[488px] left-[164px] st:top-[9px] st:left-[720px] st:right-[4px] st:bottom-[250px] "
           />
           <img
             src="/images/vector2.svg"
             alt=""
-            className="relative right-[700px] bottom-[188px] md:right-[580px] md:top-[30px] md:bottom-0"
+            className="absolute right-[30px] st:right-[700px] bottom-[220px] st:left-[520px] st:top-[140px] st:bottom-0"
           />
-        </section>
-        {isSigninModal && (
-          <SigninModal
-            setIsSigninModal={setIsSigninModal}
-            openSignupModal={openSignupModal}
-            openResetPasswordModal={openResetPasswordModal}
-          />
-        )}
-        {isSignupModal && (
-          <SignupModal
-            setIsSignupModal={setIsSignupModal}
-            openSigninModal={openSigninModal}
-          />
-        )}
-        {isResetPasswordEmailModal && (
-          <ResetPasswordEmail
-            email={email}
-            setIsResetPasswordEmailModal={setIsResetPasswordEmailModal}
-          />
-        )}
-      </main>
+        </div>
+      </section>
+      {isSigninModal && (
+        <SigninModal
+          setIsSigninModal={setIsSigninModal}
+          openSignupModal={openSignupModal}
+          openResetPasswordModal={openResetPasswordModal}
+        />
+      )}
+      {isSignupModal && (
+        <SignupModal
+          setIsSignupModal={setIsSignupModal}
+          openSigninModal={openSigninModal}
+        />
+      )}
+      {isResetPasswordEmailModal && (
+        <ResetPasswordEmail
+          email={email}
+          setIsResetPasswordEmailModal={setIsResetPasswordEmailModal}
+        />
+      )}
     </>
   );
 };
