9cc49203b613a6fc94f67e5ba7dbe064ad387954
rambak02 <rambak02@mail.ru>
2024-08-16 14:56:09 +0300

    поправил ошибка

--- src/pages/CourseInfo.tsx
+++ src/pages/CourseInfo.tsx
@@ -1,4 +1,4 @@
-import { Outlet, useParams } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import CourseInfo from "../components/CourseInfo/CourseInfo";
 
 const CourseInfoPage = () => {
